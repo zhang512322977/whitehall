7934a694bda0e9a969421019f6ec84ce17333d64
Mat Moore <mat.moore@digital.cabinet-office.gov.uk>
2017-01-26 16:17:51 +0000

    Use content-tagger's TreeNode class for the taxonomy

--- app/helpers/admin/edition_tags_helper.rb
+++ app/helpers/admin/edition_tags_helper.rb
@@ -1,12 +1,15 @@
 module Admin::EditionTagsHelper
   def checkbox_for_taxonomy(selected_taxon_content_ids, taxon)
-    checked = selected_taxon_content_ids.include?(taxon["content_id"])
+
+    checked = taxon.tree.any? do |descendant_taxon|
+      selected_taxon_content_ids.include?(descendant_taxon.content_item["content_id"])
+    end
 
     check_box_tag(
       "edition_taxonomy_tag_form[taxons][]",
-      taxon["content_id"],
+      taxon.content_item["content_id"],
       checked,
-      id: taxon["content_id"]
+      id: taxon.content_item["content_id"]
     )
   end
 end
--- lib/taxonomy.rb
+++ lib/taxonomy.rb
@@ -3,9 +3,57 @@ module Taxonomy
     @education ||= begin
       filename = Rails.root + "lib/taxonomy/education.json"
       taxonomy = JSON.parse(File.read(filename))
-      $stderr.puts taxonomy
-      $stderr.puts taxonomy["links"]["child_taxons"]
-      taxonomy
+      TreeNode.new(content_item: taxonomy)
     end
   end
+
+  # TODO: move this to a gem, so we can use the same code in content tagger
+  class TreeNode
+    attr_reader :name, :content_item, :children
+    attr_accessor :parent_node
+    delegate :content_id, to: :content_item
+    delegate :map, :each, to: :tree
+
+    def initialize(content_item:, name_field: "title")
+      @name = content_item[name_field]
+      @content_item = content_item
+      @children = []
+
+      child_taxons = content_item["links"]["child_taxons"]
+
+      if child_taxons.present?
+        child_nodes = child_taxons.map {|child| TreeNode.new(name_field: name_field, content_item: child)}
+        child_nodes.each do |child_node|
+          self << child_node
+        end
+      end
+    end
+
+    def <<(child_node)
+      child_node.parent_node = self
+      @children << child_node
+    end
+
+    def tree
+      return [self] if @children.empty?
+
+      @children.each_with_object([self]) do |child, tree|
+        tree.concat(child.tree)
+      end
+    end
+
+    def count
+      tree.count
+    end
+
+    def root?
+      parent_node.nil?
+    end
+
+    def node_depth
+      return 0 if root?
+      1 + parent_node.node_depth
+    end
+  end
+
 end
