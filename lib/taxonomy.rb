a73c2fe3267e493f345a4dac9ed47cae50ddbc6b
Mat Moore <mat.moore@digital.cabinet-office.gov.uk>
2017-01-27 10:22:28 +0000

    WIP

--- lib/taxonomy.rb
+++ lib/taxonomy.rb
@@ -22,7 +22,10 @@ module Taxonomy
       child_taxons = content_item["links"]["child_taxons"]
 
       if child_taxons.present?
-        child_nodes = child_taxons.map {|child| TreeNode.new(name_field: name_field, content_item: child)}
+        child_nodes = child_taxons.map do |child|
+          TreeNode.new(name_field: name_field, content_item: child)
+        end
+
         child_nodes.each do |child_node|
           self << child_node
         end
