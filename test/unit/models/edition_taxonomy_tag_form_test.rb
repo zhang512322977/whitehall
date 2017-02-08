260c3429072db8814adf03118c16b590a92fb67d
Mat Moore <mat.moore@digital.cabinet-office.gov.uk>
2017-02-08 17:07:51 +0000

    WIP update tests

--- test/unit/models/edition_taxonomy_tag_form_test.rb
+++ test/unit/models/edition_taxonomy_tag_form_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class EditionTaxonomyTagFormTest < ActiveSupport::TestCase
   test '#load should request links to publishing-api' do
     content_id = "64aadc14-9bca-40d9-abb6-4f21f9792a05"
-    taxons = ["df2e7a3e-2078-45de-a75a-fd37d027427e"]
+    taxons = ["c58fdadd-7743-46d6-9629-90bb3ccc4ef0"]
 
     publishing_api_has_links(
       {
@@ -22,13 +22,14 @@ class EditionTaxonomyTagFormTest < ActiveSupport::TestCase
     assert_equal(form.previous_version, 1)
   end
 
-  test '#publish should send a patch links to publishing-api' do
+  test '#publish should send a patch links to publishing-api with the most specific selected taxons' do
     content_id = "64aadc14-9bca-40d9-abb6-4f21f9792a05"
-    taxons = ["df2e7a3e-2078-45de-a75a-fd37d027427e"]
+    selected_taxons = %w(47b6ce42-0bfa-42ee-9ff1-7a9c71ee9727 c58fdadd-7743-46d6-9629-90bb3ccc4ef0)
+    most_specific_taxons = %w(47b6ce42-0bfa-42ee-9ff1-7a9c71ee9727)
 
     form = EditionTaxonomyTagForm.new(
       edition_content_id: content_id,
-      taxons: taxons,
+      selected_taxons: selected_taxons,
       previous_version: 1,
     )
 
@@ -38,7 +39,30 @@ class EditionTaxonomyTagFormTest < ActiveSupport::TestCase
       content_id,
       {
         links: {
-          taxons: ["df2e7a3e-2078-45de-a75a-fd37d027427e"],
+          taxons: most_specific_taxons,
+        },
+        previous_version: 1
+      }
+    )
+  end
+
+  test '#publish ignores links to non-taxonomy content items' do
+    content_id = "64aadc14-9bca-40d9-abb6-4f21f9792a05"
+    taxons = ["aaaaaaaa-7743-46d6-9629-90bb3ccc4ef0"]
+
+    form = EditionTaxonomyTagForm.new(
+      edition_content_id: content_id,
+      selected_taxons: taxons,
+      previous_version: 1,
+    )
+
+    form.publish!
+
+    assert_publishing_api_patch_links(
+      content_id,
+      {
+        links: {
+          taxons: [],
         },
         previous_version: 1
       }
